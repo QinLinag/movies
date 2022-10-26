957c2092442b3991460f94dcd02274d7b37d5dd9
秦亮 <23>
2022-10-26 21:19:24 +0800

    gateway提交

---
+++ movies-gateway/src/main/java/com/cqupe/movies/gateway/MoviesGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.cqupe.movies.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MoviesGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesGatewayApplication.class, args);
+    }
+
+}
---
+++ movies-gateway/src/test/java/com/cqupe/movies/gateway/MoviesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqupe.movies.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
